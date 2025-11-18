ffb4ce538c97aa0ad257d6198b9354372449e8dd
Dimitrios Chatziioannou <52290418+triatetarta@users.noreply.github.com>
2025-11-18 18:46:58 +0000

    adding collapsible (#56)
    Co-authored-by: Dimitrios Chatziioannou <dimitrios@kmatech.co>

--- registry/__index__.tsx
+++ registry/__index__.tsx
@@ -126,6 +126,30 @@ export const Index: Record<string, any> = {
     categories: undefined,
     meta: undefined,
   },
+  collapsible: {
+    name: 'collapsible',
+    description: '',
+    type: 'registry:ui',
+    registryDependencies: undefined,
+    files: [
+      {
+        path: 'registry/default/ui/collapsible.tsx',
+        type: 'registry:ui',
+        target: '',
+      },
+    ],
+    component: React.lazy(async () => {
+      const mod = await import('@/registry/default/ui/collapsible.tsx');
+      const exportName =
+        Object.keys(mod).find(
+          (key) =>
+            typeof mod[key] === 'function' || typeof mod[key] === 'object'
+        ) || item.name;
+      return { default: mod.default || mod[exportName] };
+    }),
+    categories: undefined,
+    meta: undefined,
+  },
   input: {
     name: 'input',
     description: '',
