95ceafca6e6e4db58db835edf4f8dc01f2e16613
Dimitrios Chatziioannou <dimitrios@kmatech.co>
2025-11-18 08:50:32 +0000

    update fumadocs

--- lib/source.ts
+++ lib/source.ts
@@ -1,6 +1,6 @@
-import { docs } from '@/.source';
 import { loader, type InferPageType } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { docs } from 'fumadocs-mdx:collections/server';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
